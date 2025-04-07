6c8b4180cecf94e7787f204d50bf614c4c3a6347
Nathaniel J Thai <nathanj.thai@gmail.com>
2025-04-07 18:06:24 -0400

    temp img added

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,5 +1,5 @@
 import '../index.css'
-
+import cat from '../assets/catcatcat.gif'
 function Landing() {
   return (
     <>
@@ -9,11 +9,13 @@ function Landing() {
           <p> </p>
         </div>
         <div className="align-middle flex flex-col justify-between items-center">
+          {/*placeholder*/}
+          <img src={cat}></img>
           <h1 className="text-4xl md:text-6xl lg:text-8xl">Hack the Heights 2025</h1>
           <p className="text-lg md:text-xl lg:text-2xl">Notify me when more information comes out!</p>
           <form className="flex mt-4">
-            <input type="email" placeholder="your email here" required className="bg-backgroundCream mx-5 text-fontPurple rounded-xl" />
-            <button type="submit" className="bg-backgroundCream text-fontPurple mx-5 rounded-xl">send</button>
+            <input type="email" placeholder="your email here" required className="bg-backgroundCream mx-5 px-2 text-fontPurple rounded-xl" />
+            <button type="submit" className="bg-backgroundCream text-fontPurple mx-2 px-2 rounded-xl">send</button>
           </form>
         </div>
         <footer className="flex align-bottom justify-between w-full px-8 mb-4 text-sm md:text-base lg:text-lg">
