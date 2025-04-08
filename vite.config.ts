0e94e9ae8bec6ecb8aac430340d8cd47a8a9f35c
Hunter S <67876717+hunterschep@users.noreply.github.com>
2025-04-07 22:01:16 -0400

    baka

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hth2025/',
   plugins: [
     react(),
     tailwindcss()
   ],
 })
+
