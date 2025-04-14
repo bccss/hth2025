83118cf9ad7edde2d78519ffa52a4ca52bd92d66
Nathaniel J Thai <nathanj.thai@gmail.com>
2025-04-14 19:24:36 -0400

    about

---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+import '../index.css'
+
+function About() {
+   return (
+      <>
+         <div className="flex flex-col items-center">
+            <h1>About</h1>
+            <p>mm food</p>
+         </div>
+      </>
+   )
+}
+
+export default About
